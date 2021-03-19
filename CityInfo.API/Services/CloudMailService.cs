6ca8dd6df51c81606a307c64b24e23c12e37d814
Ryan Wire <ryanwire@outlook.com>
2021-03-19 21:15:05 +0300

    Creates an IMailService interface for dependency injection

--- CityInfo.API/Services/CloudMailService.cs
+++ CityInfo.API/Services/CloudMailService.cs
@@ -3,7 +3,7 @@ using System.Diagnostics;
 
 namespace CityInfo.API.Services
 {
-    public class LocalMailService : IMailService
+    public class CloudMailService : IMailService
     {
         private string _mailTo = "admin@mycompany.com";
         private string _mailFrom = "noreply@mycompany.com";
@@ -11,7 +11,7 @@ namespace CityInfo.API.Services
         public void Send(string subject, string message)
         {
             // send mail - output to debug window
-            Debug.WriteLine($"Mail from {_mailFrom} to {_mailTo}, with LocalMailService.");
+            Debug.WriteLine($"Mail from {_mailFrom} to {_mailTo}, with CloudMailService.");
             Debug.WriteLine($"Subject: {subject}");
             Debug.WriteLine($"Message: {message}");
         }
--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -30,8 +30,11 @@ namespace CityInfo.API
             //        castedResolver.NamingStrategy = null;
             //    }
             //});
-
-            services.AddTransient<LocalMailService>();
+#if DEBUG
+            services.AddTransient<IMailService, LocalMailService>();
+#else
+            services.AddTransient<IMailService, CloudMailService>();
+#endif
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
