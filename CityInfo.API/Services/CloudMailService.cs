368421ae7e14b478ce2b816e0b57511d5654aca9
Ryan Wire <ryanwire@outlook.com>
2021-03-19 21:26:19 +0300

    Sets up a configuration file

--- CityInfo.API/Services/CloudMailService.cs
+++ CityInfo.API/Services/CloudMailService.cs
@@ -1,17 +1,22 @@
 ﻿using System;
 using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
 
 namespace CityInfo.API.Services
 {
     public class CloudMailService : IMailService
     {
-        private string _mailTo = "admin@mycompany.com";
-        private string _mailFrom = "noreply@mycompany.com";
+        private readonly IConfiguration _configuration;
+
+        public CloudMailService(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
 
         public void Send(string subject, string message)
         {
             // send mail - output to debug window
-            Debug.WriteLine($"Mail from {_mailFrom} to {_mailTo}, with CloudMailService.");
+            Debug.WriteLine($"Mail from {_configuration["mailSettings:mailFromAddress"]} to {_configuration["mailSettings:mailToAddress"]}, with CloudMailService.");
             Debug.WriteLine($"Subject: {subject}");
             Debug.WriteLine($"Message: {message}");
         }
