780ad8b0f3828bad52e6a499850ebcd384d0ba0b
Ryan Wire <ryanwire@outlook.com>
2021-03-19 23:48:35 +0300

    Sets up the repository pattern

---
+++ CityInfo.API/Services/ICityInfoRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CityInfo.API.Entities;
+
+namespace CityInfo.API.Services
+{
+    public interface ICityInfoRepository
+    {
+        IEnumerable<City> GetCities();
+
+        City GetCity(int cityId, bool includePointsOfInterest);
+
+        IEnumerable<PointOfInterest> GetPointsOfInterestForCity(int cityId);
+
+        PointOfInterest GetPointOfInterestForCity(int cityId, int pointOfInterestId);
+    }
+}
