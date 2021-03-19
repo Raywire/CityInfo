c22d1b015787b19ab02a9e5fc7f4043d862f7b35
Ryan Wire <ryanwire@outlook.com>
2021-03-19 13:45:41 +0300

    Sets up formatters and content negotiation to support xml

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json.Serialization;
 
@@ -16,13 +17,18 @@ namespace CityInfo.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().AddJsonOptions(o => {
-                if(o.SerializerSettings.ContractResolver != null)
+            services.AddMvc()
+                .AddMvcOptions(o =>
                 {
-                    var castedResolver = o.SerializerSettings.ContractResolver as DefaultContractResolver;
-                    castedResolver.NamingStrategy = null;
-                }
-            });
+                    o.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+                });
+                //.AddJsonOptions(o => {
+                //    if(o.SerializerSettings.ContractResolver != null)
+                //    {
+                //        var castedResolver = o.SerializerSettings.ContractResolver as DefaultContractResolver;
+                //        castedResolver.NamingStrategy = null;
+                //    }
+                //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
