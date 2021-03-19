855596023732d92c3a304dda698eaf81fa1ce94b
Ryan Wire <ryanwire@outlook.com>
2021-03-19 13:02:02 +0300

    Adds an in-memory data store and an action to get single city - Retrieves data from the in-memory data store

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Controllers
@@ -12,10 +13,13 @@ namespace CityInfo.API.Controllers
         [HttpGet]
         public JsonResult GetCities()
         {
-            return new JsonResult(new List<object>() {
-                new { id = 1, Name = "New York City" },
-                new { id = 2, Name = "Nairobi" }
-            });
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id));
         }
     }
 }
